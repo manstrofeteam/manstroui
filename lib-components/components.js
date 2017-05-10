bd5e7ce144f6b3cfbb5d3ccc63314cb2052d108a
cainiaozone <cainiaozone@163.com>
2017-05-10 10:47:40 +0800

    增加评分组件

--- lib-components/components.js
+++ lib-components/components.js
@@ -103,6 +103,62 @@ function checkStrong(sValue) {
     }
 }
 
+/*========评分开始=======*/
+var score = "";
+window.onload = function() {
+        var oStar = document.getElementById("ms-star");
+        var aLi = oStar.getElementsByTagName("li");
+        var oUl = oStar.getElementsByTagName("ul")[0];
+        var oSpan = oStar.getElementsByTagName("span")[0];
+        var oP = oStar.getElementsByTagName("p")[0];
+        var i = iScore = iStar = 0;
+        var aMsg = [
+            "很不满意",
+            "不满意",
+            "一般",
+            "满意",
+            "非常满意"
+        ]
+
+        for (i = 1; i <= aLi.length; i++) {
+            aLi[i - 1].index = i;
+            //鼠标移过显示分数
+            aLi[i - 1].onmouseover = function() {
+                fnPoint(this.index);
+                //浮动层显示
+                oP.style.display = "block";
+                //计算浮动层位置
+                oP.style.left = oUl.offsetLeft + this.index * this.offsetWidth - 58 + "px";
+                //匹配浮动层文字内容
+                oP.innerHTML = "<em><b>" + this.index + "</b> 分 " + aMsg[this.index - 1] + "</em>"
+                    //+ aMsg[this.index - 1].match(/\|(.+)/)[1]
+            };
+
+            //鼠标离开后恢复上次评分
+            aLi[i - 1].onmouseout = function() {
+                fnPoint();
+                //关闭浮动层
+                oP.style.display = "none"
+            };
+            //点击后进行评分处理
+            aLi[i - 1].onclick = function() {
+                iStar = this.index;
+                oP.style.display = "none";
+                oSpan.innerHTML = "<strong>" + (this.index) + " 分</strong> (" + aMsg[this.index - 1] + ")";
+                score = this.index;
+                $("#score").val(score);
+                $('#score').trigger("validate");
+            }
+        }
+        //评分处理
+        function fnPoint(iArg) {
+            //分数赋值
+            iScore = iArg || iStar;
+            for (i = 0; i < aLi.length; i++) aLi[i].className = i < iScore ? "on" : "";
+        }
+    }
+    /*========评分结束=======*/
+
 $(function() {
     // 点击遮罩层本身隐藏
     $(".ms-overlay").click(function() {
